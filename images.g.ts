f84dd11c69f6dcc7198089297d10311fe00551a8
ayanRy <92228722+ayanRy@users.noreply.github.com>
2021-10-09 22:53:11 +0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Dave":return img`
+. . . . f f f f f f . . . . . . 
+. . . f e e e e e e f . . . . . 
+. . f e e e d d d f . . . . . . 
+. . . f d d d 1 8 d f . . . . . 
+. . . f d d d d d d f . . . . . 
+. . . . f d d d d f . . . . . . 
+. . f f f f f f f f f f f . . . 
+. f 4 4 4 4 4 4 4 4 f 4 4 f . . 
+f 4 f f f 4 4 4 4 4 f f f 4 f . 
+f 4 f . f 4 4 4 4 4 f . f 4 f . 
+f 4 f . f 4 4 4 4 4 f . f 4 f . 
+f 4 f . f f f f f f f . f 4 f . 
+f 4 f . f 8 f . f 8 f . f 4 f . 
+. f . . f 8 f . f 8 f . f 1 1 f 
+. . . . f 8 f . f 8 f . f 1 1 f 
+. . . . f f f . f f f . f f f f 
+`;
+            case "image2":
+            case "evil man":return img`
+9 9 9 9 9 9 d d d 9 9 9 9 9 9 9 
+9 9 9 9 9 d d d 1 1 9 9 9 9 9 9 
+9 9 9 9 9 d f d d d 9 9 9 9 9 9 
+9 9 9 9 9 d d d d d 9 9 9 9 9 9 
+9 9 9 9 9 1 d d d 9 9 9 9 9 9 9 
+9 9 9 9 9 1 2 2 2 2 9 9 9 9 9 9 
+9 9 9 9 9 1 2 2 2 2 2 9 9 9 9 9 
+9 9 9 9 9 d 2 2 2 2 2 9 9 9 9 9 
+9 9 9 d d d 2 2 2 2 2 9 9 9 9 9 
+9 9 d d 9 9 2 2 2 2 2 9 9 9 9 9 
+9 9 e 9 9 9 f f 5 f f 9 9 9 9 9 
+9 9 e 9 9 9 8 8 8 8 8 9 9 9 9 9 
+9 9 e 9 9 9 8 9 9 9 8 9 9 9 9 9 
+9 9 e 9 9 9 8 9 9 9 8 9 9 9 9 9 
+9 9 e 9 9 9 8 9 9 9 8 9 9 9 9 9 
+9 9 f 9 9 9 f 9 9 9 f 9 9 9 9 9 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
